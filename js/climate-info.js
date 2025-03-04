8e209ce5cfd8b2fd6b2a4541655deb153daf8406
leighyyy <112703412+leighyyy@users.noreply.github.com>
2025-03-04 15:41:03 +0800

    Add files via upload

---
+++ js/climate-info.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.querySelectorAll(".read-more").forEach(link => {
+//         link.addEventListener("click", function () {
+//             let extraText = this.previousElementSibling.querySelector(".extra-text");
+//             if (extraText.style.display === "inline") {
+//                 extraText.style.display = "none";
+//                 this.innerHTML = "Read More";
+//             } else {
+//                 extraText.style.display = "inline";
+//                 this.innerHTML = "Read Less";
+//             }
+//         });
+//     });
+// });
